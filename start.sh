35649feec000e6f8248f4782ee0a17b2b89e473a
Dominik Grzelak <dominik.grzelak@tu-dresden.de>
2022-12-06 08:23:31 +0100

    updated CDO to version 4.20

---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker compose up
+#docker run -ti --rm cdo-410-stgitlab-cdo-server /root/cdo-v4.20/eclipse/eclipse -debug -consoleLog
+#docker run -ti --rm --detach cdo-410-stgitlab-cdo-server /root/cdo-v4.20/eclipse/eclipse -debug -consoleLog
